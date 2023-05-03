25d68188d4db9c26b9b8f459bc2c8bf77eda3eff
SeiyaKobayashi <seiya.kobayashi.work@gmail.com>
2023-05-03 21:22:37 +0900

    Fixed script for encoding testing results

--- provers/scripts/encode-testing-results.ts
+++ provers/scripts/encode-testing-results.ts
@@ -1,32 +1,19 @@
 #! ./node_modules/.bin/ts-node
 
 import 'zx/globals';
-import { StandardMerkleTree } from '@openzeppelin/merkle-tree';
 import * as tf from '@tensorflow/tfjs-node';
 
 import dataDriver from "../utils/load-mnist-data";
+import merkleTreeDriver from "../utils/merkle-tree";
 
 const MODEL: string = argv.MODEL || "demo-tfjs";
+const OUTPUT_FILE: string = argv.OUTPUT_FILE || "demo-tree";
 // const TESTING_DATA: string = argv.TESTING_DATA || "demo";
-const TEST_DATA_SIZE: number = 28 * 28;
-
-const generateMerkleTree = (
-  _testData: any,
-  _predictions: any,
-): string => {
-  const numOfLeaves: number = _testData.size / TEST_DATA_SIZE;
-  const leaves: string[][] = [];
-  for (let i = 0; i < numOfLeaves; i++) {
-    leaves.push([
-      JSON.stringify(_testData.dataSync().slice(i * TEST_DATA_SIZE, i * TEST_DATA_SIZE + TEST_DATA_SIZE)),
-      _predictions[i],
-    ]);
-  }
-
-  return StandardMerkleTree.of(leaves, ['string', 'uint256']).root;
-};
+const DIFFICULTY: number = 10;
 
 (async () => {
+  const mtDriver = new merkleTreeDriver(DIFFICULTY);
+
   // load model
   console.log('\nLoading model...');
   const model = await tf.loadLayersModel(`file://../models/${MODEL}/model.json`);
@@ -42,6 +29,7 @@ const generateMerkleTree = (
 
   // encoding testing results as a Merkle tree
   console.log('\nEncoding testing results...');
-  const merkleRoot = generateMerkleTree(testImages, predictions);
+  const merkleRoot = mtDriver.generateMerkleTree(testImages, predictions, `${OUTPUT_FILE}.json`);
+
   console.log(`\nmerkleRoot: ${merkleRoot}\n`);
 })();
