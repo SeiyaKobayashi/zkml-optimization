ba899e982fb216a5eb9efd4bbef850b4fbfdc55e
SeiyaKobayashi <seiya.kobayashi.work@gmail.com>
2023-05-03 23:59:24 +0900

    Fix script for generating Merkle proofs

--- provers/scripts/generate-merkle-proofs.ts
+++ provers/scripts/generate-merkle-proofs.ts
@@ -1,9 +1,13 @@
 #! ./node_modules/.bin/ts-node
 
 import 'zx/globals';
+import * as tf from '@tensorflow/tfjs-node';
+
 import merkleTreeDriver from "../utils/merkle-tree";
+import circuitJson from '../../circuits/demo-circuit.json' assert { type: "json" }
 
 const MERKLE_TREE: string = argv.MERKLE_TREE || "demo-tree";
+const INPUT_FILE: string = argv.INPUT_FILE || "demo";
 const DIFFICULTY: number = 10;
 
 (async () => {
@@ -17,9 +21,15 @@ const DIFFICULTY: number = 10;
   console.log('\nSearching Merkle tree...');
   const {values, indices} = mtDriver.searchMerkleTree(tree, '0x3b1', DIFFICULTY);
 
+  // save input data of circuit as .json file
+  console.log('\nSaving input data as a JSON file...');
+  const valuesJson: number[] = Object.values(JSON.parse(values[0][0]));
+  fs.writeFileSync(
+    `${INPUT_FILE}-input.json`,
+    JSON.stringify({...circuitJson, ...{'in': tf.tensor3d(valuesJson, [28, 28, 1]).mul(10 ** 17).arraySync()}})
+  );
+
   // generate Merkle proofs of the specified leaves
   console.log('\nGenerating Merkle proofs...\n');
   const { proof, proofFlags, leaves } = mtDriver.generateMerkleProofs(tree, indices);
-
-  console.log(proof);
 })();
